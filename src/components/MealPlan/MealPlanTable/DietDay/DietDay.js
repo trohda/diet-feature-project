7e198c70f2ae8b502b089a1562ede6572df45593
Tomasz Rohda <tomaszrohda@wp.pl>
2022-06-09 19:16:00 +0200

    Print Done

--- src/components/MealPlan/MealPlanTable/DietDay/DietDay.js
+++ src/components/MealPlan/MealPlanTable/DietDay/DietDay.js
@@ -6,10 +6,13 @@ import {
 import barbellsGrey from "../../img/barbellsGrey.png";
 import barbellsOrange from "../../img/barbellsOrange.png";
 import workoutCheck from "../../img/workoutCheck.png";
+import printer from "../../img/printer.png";
 import { useState } from "react";
+import Meal from "./Meal/Meal";
 
 const DietDay = ({ dayData }) => {
   const [workoutDone, setWorkoutDone] = useState(false);
+  //   console.log(dayData.meals);
 
   const handlerWorkoutDone = (e) => {
     e.preventDefault();
@@ -19,10 +22,18 @@ const DietDay = ({ dayData }) => {
     <>
       <DietDayWrapper>
         <DietDayHeader>{`Day ${dayData.dayNumber}`}</DietDayHeader>
-
+        {dayData.meals.map()}
+        <Meal />
         <DietDayFooter>
           {dayData.isGuiltFree ? (
-            <div className="print"></div>
+            <>
+              <div className="print">
+                <div className="printerImage">
+                  <img src={printer} alt="printer" />
+                </div>
+                <div className="printerText">Print</div>
+              </div>
+            </>
           ) : (
             <>
               <div className="carbs">
@@ -36,7 +47,7 @@ const DietDay = ({ dayData }) => {
                         className="doneBarbells"
                         src={barbellsOrange}
                         alt="barbell"
-                      />{" "}
+                      />
                       <img src={workoutCheck} alt="checked mark" />
                     </>
                   ) : (
